9a3b2c0be356e069e395279f824806677bf4471a
Alex Schedler <con.sume.org+github@googlemail.com>
2020-08-20 12:51:08 +0200

    Update bootstrap.js

--- sql/bootstrap.js
+++ sql/bootstrap.js
@@ -1,6 +1,16 @@
 const fs = require('fs')
+const url = require('url');
 const postgres = require('postgres')
-const sql = postgres(process.env.DATABASE_URL) 
+
+const con = new URL(process.env.DATABASE_URL);
+const cfg = {
+    user: con.username,
+    password: con.password,
+    database: con.pathname.substring(1),
+    port: con.port,
+    host: con.hostname
+}
+const sql = postgres(cfg) 
 const com = fs.readFileSync('./sql/schema.postgresql.sql')
 
 sql`${com}`
